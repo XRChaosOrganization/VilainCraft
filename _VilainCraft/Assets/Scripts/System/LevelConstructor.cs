7456662c7d0538c0eae941a5652e1cf179916b21
Ben <benjamin.matheron@hotmail.fr>
2022-06-10 00:39:07 +0200

    [Tool] LevelConstructor v1.1
    - Takes Image input to generate map
    - No height differentials yet
    - Only one layer used for now

--- _VilainCraft/Assets/Scripts/System/LevelConstructor.cs
+++ _VilainCraft/Assets/Scripts/System/LevelConstructor.cs
@@ -3,30 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+
+public class CellData
+{
+    public Vector3 position;
+    public int height;
+
+    public CellData(Vector3 _pos, int _height)
+    {
+        position = _pos;
+        height = _height;
+    }
+}
+
+
 public class LevelConstructor : MonoBehaviour
 {
     public int tileSize;
 
     public GameObject tilePrefab;
     public Transform tileContainer;
-    public Vector2 terrainSize;
+    [HideInInspector]public Texture2D mapData;
     [Range(0, 100)] public int variance;
 
 
     public void GenerateTerrain()
     {
-        for (int x = 0; x < terrainSize.x; x++)
+        if (tileContainer.childCount > 0)
+            ClearTerrain();
+
+
+        List<CellData> _mapData = new List<CellData>();
+        for (int x = 0; x < mapData.width; x++)
         {
-            for (int y = 0; y < terrainSize.y; y++)
+            for (int y = 0; y < mapData.height; y++)
             {
                 Vector3 position = new Vector3(x * tileSize, 0f, y * tileSize);
 
+                Color color = mapData.GetPixel(x, y);
+                int height = color.r > 0 ? 1 : 0;
+                _mapData.Add(new CellData(position, height));
+            }
+        }
+
+        foreach (var item in _mapData)
+        {
+            if (item.height > 0)
+            {
                 int direction = Random.Range(0, 4);
                 float varianceCheck = Random.Range(0f, 1f);
                 int variantIndex = Random.Range(1, 4);
 
                 GameObject instance = PrefabUtility.InstantiatePrefab(tilePrefab, tileContainer) as GameObject;
-                instance.transform.position = position;
+                instance.transform.position = item.position;
                 instance.transform.rotation = Quaternion.Euler(0f, 90f * direction, 0f);
 
                 if (varianceCheck <= (float)variance / 100)
@@ -34,9 +63,8 @@ public class LevelConstructor : MonoBehaviour
                     MeshRenderer[] mr = instance.GetComponentsInChildren<MeshRenderer>();
                     mr[variantIndex].enabled = true;
                 }
-
-
             }
+
         }
     }
 
@@ -53,6 +81,7 @@ public class LevelConstructor : MonoBehaviour
 }
 
 
+
 [CustomEditor(typeof(LevelConstructor))]
 public class LevelEditor : Editor
 {
@@ -60,6 +89,12 @@ public class LevelEditor : Editor
     {
         base.OnInspectorGUI();
         LevelConstructor constructor = (LevelConstructor)target;
+
+       
+        constructor.mapData = (Texture2D)EditorGUILayout.ObjectField("Map Data", constructor.mapData, typeof(Texture2D), false);
+
+        
+
 
         GUILayout.Space(14);
         GUI.backgroundColor = Color.green;
